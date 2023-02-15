411db775e4589efd2172d193b8a1370bc07c4bf1
James Hoffman <jamesvhoffman@outlook.com>
2023-02-14 23:32:36 -0500

    Changed from tabs to spaces

--- slash/info.js
+++ slash/info.js
@@ -2,24 +2,24 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder().setName("info").setDescription("Displays info about the currently playing song"),
-	run: async ({ client, interaction }) => {
-		const queue = client.player.getQueue(interaction.guildId);
+  data: new SlashCommandBuilder().setName("info").setDescription("Displays info about the currently playing song"),
+  run: async ({ client, interaction }) => {
+    const queue = client.player.getQueue(interaction.guildId);
 
-		if (!queue) return await interaction.editReply("There are no songs in the queue");
+    if (!queue) return await interaction.editReply("There are no songs in the queue");
 
-		let bar = queue.createProgressBar({
-			queue: false,
-			length: 19,
-		});
+    let bar = queue.createProgressBar({
+      queue: false,
+      length: 19,
+    });
 
-		const song = queue.current;
+    const song = queue.current;
 
-		await interaction.editReply({
-			embeds: [new EmbedBuilder()
-				.setThumbnail(song.thumbnail)
-				.setDescription(`Currently Playing [${song.title}](${song.url})\n\n` + bar)
-			],
-		});
-	},
+    await interaction.editReply({
+      embeds: [new EmbedBuilder()
+        .setThumbnail(song.thumbnail)
+        .setDescription(`Currently Playing [${song.title}](${song.url})\n\n` + bar)
+      ],
+    });
+  },
 };
--- slash/skip.js
+++ slash/skip.js
@@ -2,19 +2,19 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder().setName("skip").setDescription("Skips the current song"),
-    run: async ({ client, interaction }) => {
-        const queue = client.player.getQueue(interaction.guildId);
+  data: new SlashCommandBuilder().setName("skip").setDescription("Skips the current song"),
+  run: async ({ client, interaction }) => {
+    const queue = client.player.getQueue(interaction.guildId);
 
-        if (!queue) return await interaction.editReply("There are no songs in the queue");
+    if (!queue) return await interaction.editReply("There are no songs in the queue");
 
-        const currentSong = queue.current;
+    const currentSong = queue.current;
 
-        queue.skip();
-        await interaction.editReply({
-            embeds: [
-                new EmbedBuilder().setDescription(`${currentSong.title} has been skipped!`).setThumbnail(currentSong.thumbnail)
-            ]
-        });
-    },
+    queue.skip();
+    await interaction.editReply({
+      embeds: [
+        new EmbedBuilder().setDescription(`${currentSong.title} has been skipped!`).setThumbnail(currentSong.thumbnail)
+      ]
+    });
+  },
 };
